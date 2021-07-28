7c218f32ada9b46927eb625295789c1c833e6f73
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-28 11:31:22 +0200

    Trivial C

---
+++ C/solving_for_carrots.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{   
+    int rows;
+    int points;
+    
+    scanf("%d%d", &rows, &points);
+    for (int i = 0; i < rows; i++){
+        char name[1000];
+        scanf("%s", &name);
+    }
+    
+    printf("%d",points);
+    
+    return 0;
+}
