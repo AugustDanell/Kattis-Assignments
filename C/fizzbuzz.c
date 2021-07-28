db54725257b5d7351a8f1b2bd8f162a73560674a
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-28 11:02:16 +0200

    Repeating C programming

---
+++ C/fizzbuzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{   
+    int x;
+    int y;
+    int n;
+    
+    scanf("%d%d%d", &x, &y, &n);
+    
+    for (int i = 1; i <= n; i++){
+        if(i % x == 0 && i % y == 0){
+            printf("FizzBuzz");
+        }
+        else if(i % x == 0){
+            printf("Fizz");
+        }
+        else if(i % y == 0){
+            printf("Buzz");
+        }
+        else{
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
