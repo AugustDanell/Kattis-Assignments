cbbe5b0b08a8909956506725c626c8427d7683bb
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-01 18:27:39 +0200

    Tail_recursive implementation of faculty in C.

---
+++ C/last_factorial_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int tail_factorial(n,tail){
+    if(n == 1 || n == 0){
+        return tail;
+    }
+    else{
+        tail *= n;
+        return tail_factorial(n-1, tail);
+    }
+}
+
+
+int main()
+{
+    int testcases = 0;
+    scanf("%d",&testcases);
+    
+    for(int i = 0; i < testcases; i++){
+        int fac = 0;
+        scanf("%d", &fac);
+        int factorial = tail_factorial(fac, 1);
+        int mod = factorial % 10;
+        printf("%d\n",mod);
+    }
+
+    return 0;
+}
