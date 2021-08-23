9b3e6fbf6b7dd2765b52159a9f1e49aa56522a77
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-23 14:17:51 +0200

    Create Faktor.cs

---
+++ C#/Faktor.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {   
+        string input;
+        input = Console.ReadLine();
+        string[] split = input.Split(' ');
+        
+        long a = Int64.Parse(split[0]);
+        long b = Int64.Parse(split[1]);
+        
+        Console.WriteLine(a*(b-1)+1);
+    }
+}
