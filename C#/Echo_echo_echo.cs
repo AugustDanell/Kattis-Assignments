1d4042dccc18d6b5b95c2b863953cad1d34b48d9
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-23 15:07:24 +0200

    Trivial C# commit.

---
+++ C#/Echo_echo_echo.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System;
+
+class Program
+{   
+    
+    
+    static void Main()
+    {   
+        string input = Console.ReadLine();
+        string echo = input + " " + input + " " + input;
+        
+        Console.WriteLine(echo);
+        
+    }
+}
