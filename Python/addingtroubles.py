21ccaf6065733b50344273f4c720e9e6327427e1
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2023-04-02 13:28:50 +0200

    556. Trivial Problem.

---
+++ Python/addingtroubles.py
@@ -0,0 +1,6 @@
+# A solution to the trivial problem: https://open.kattis.com/problems/addingtrouble
+l=list(map(int,input().split()))
+if(l[0] + l[1] == l[2]):
+    print("correct!")
+else:
+    print("wrong!")
