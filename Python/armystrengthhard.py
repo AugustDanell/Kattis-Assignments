fc422e76db1bca4b8a46e3f3e834bdee41f77c32
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2023-04-04 21:20:16 +0200

    604th Kattis.

---
+++ Python/armystrengthhard.py
@@ -0,0 +1,15 @@
+# A solution to the problem: https://open.kattis.com/problems/armystrengthhard
+
+testcases = int(input())
+for i in range(testcases):
+    input()
+    Ng,Nm = list(map(int,input().split()))
+    Godzilla = list(map(int,input().split()))
+    Mech = list(map(int,input().split()))
+
+    Godzilla = sorted(Godzilla, reverse=True)
+    mech = sorted(Mech, reverse=True)
+    if(Godzilla[0] >= mech[0]):
+        print("Godzilla")
+    else:
+        print("MechaGodzilla")
