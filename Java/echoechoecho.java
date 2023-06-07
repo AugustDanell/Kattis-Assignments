0e983db83d53341d3ff3cb6503b8773a997f13af
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2023-06-07 16:51:05 +0200

    Repeating java.

---
+++ Java/echoechoecho.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class echoechoecho{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        System.out.print(message + " " + message + " " + message);
+    }
+}
